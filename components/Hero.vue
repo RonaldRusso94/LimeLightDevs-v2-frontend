a42e0a330c26247e95f032ce4bce755054390a75
Zeeshan <084zeeshankhan@gmail.com>
2021-01-15 00:34:58 +0530

    make pageIntro a generic component.

--- components/Hero.vue
+++ components/Hero.vue
@@ -12,18 +12,17 @@
     <div
       class="relative lg:w-1/2 flex flex-col items-start lg:ml-56 my-24 lg:my-36 pl-8 lg:pl-0"
     >
-      <span class="text-6xl md:text-5xl font-hibana mb-4 lg:mb-2"
-        >Welcome!</span
-      >
-      <span class="text-3xl font-hibana text-app-green-1 mb-4 lg:mb-8"
+      <span class="text-6xl md:text-5xl font-hibana mb-4 lg:mb-2">{{
+        getTitle
+      }}</span>
+      <span
+        :class="[getName === 'contact' ? 'block' : 'hidden']"
+        class="text-3xl font-hibana text-app-green-1 mb-4 lg:mb-8"
         >Limelight - Web Developing</span
       >
-      <p class="text-md lg:text-lg w-5/6 mb-8">
-        Lorem ipsum dolor sit amet consectetur adipiscing elit, fames semper
-        vehicula velit dapibus in iaculis. Eros cursus lobortis leo hendrerit
-        faucibus parturient sagittis justo curabitur lacinia ridiculus class.
-      </p>
+      <p class="text-md lg:text-lg w-5/6 mb-8">{{ getDesc }}</p>
       <a
+        :class="[getName === 'contact' ? 'block' : 'hidden']"
         href="#contact"
         class="px-5 md:px-8 py-2 tracking-wider font-bold text-xl text-app-green-1 border-2 transition duration-200 border-app-green-1 rounded-full cursor-pointer focus:outline-none hover:text-white hover:bg-app-green-1"
       >
@@ -35,6 +34,7 @@
         class="flex h-full items-center justify-center mt-8 lg:mt-0 lg:absolute lg:left-0 pointer-events-none"
       >
         <span
+          :class="[getName === 'contact' ? 'block' : 'hidden']"
           class="text-app-gray-3 font-bold text-lg transform lg:-rotate-90 lg:-translate-x-32 lg:-translate-y-10"
           >contact@limelight.com</span
         >
@@ -42,3 +42,23 @@
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  props: {
+    type: Object,
+  },
+
+  computed: {
+    getTitle() {
+      return this.type.title
+    },
+    getDesc() {
+      return this.type.desc
+    },
+    getName() {
+      return this.type.name
+    },
+  },
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,23 @@
 <template>
   <div>
-    <Hero />
+    <Hero :type="contact" />
     <Packages />
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      contact: {
+        name: 'contact',
+        title: 'Welcome!',
+        desc:
+          'Lorem ipsum dolor sit amet consectetur adipiscing elit, fames semper vehicula velit dapibus in iaculis. Eros cursus lobortis leo hendrerit faucibus parturient sagittis justo curabitur lacinia ridiculus class',
+      },
+    }
+  },
+}
 </script>
 
 <style></style>
