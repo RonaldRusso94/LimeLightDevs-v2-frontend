69b2ad8cd6df07c1edefe9277e497efb8688da22
Zeeshan <084zeeshankhan@gmail.com>
2021-01-15 01:30:59 +0530

    clean index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -2,18 +2,13 @@
   <div>
     <Hero />
     <Packages />
-    <PageIntro :desc="desc" :title="title" />
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {
-      desc:
-        'Lorem ipsum dolor sit amet consectetur adipiscing elit, fames semper vehicula velit dapibus in iaculis. Eros cursus lobortis leo hendrerit faucibus parturient sagittis justo curabitur lacinia ridiculus class.',
-      title: 'Welcome!',
-    }
+    return {}
   },
 }
 </script>
