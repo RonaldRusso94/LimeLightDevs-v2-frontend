961b4f60ed3b4b175800f602236b8062ff08e616
Zeeshan khan <zeeshan@fastapp.in>
2021-07-25 00:33:26 +0530

    replace png contact image with svg version

--- components/Contact.vue
+++ components/Contact.vue
@@ -40,12 +40,15 @@
               object-contain
               z-20
               w-1/2
-              lg:w-3/4
+              lg:w-4/6
               max-h-3/4
               transform
               -rotate-12
+              translate-x-4
+              lg:translate-x-12
+              xl:translate-x-16
             "
-            src="/contact/OSXDBL1_2.png"
+            src="/contact/OSXDBL1_2.svg"
           />
         </div>
         <div
