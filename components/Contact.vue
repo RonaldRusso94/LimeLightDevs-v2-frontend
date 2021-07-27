9c686fc6bb4e706431e6f0aebea83aef62ef2167
Zeeshan khan <zeeshan@fastapp.in>
2021-07-27 14:10:31 +0530

    fix contact image overflow issue

--- components/Contact.vue
+++ components/Contact.vue
@@ -40,13 +40,12 @@
               object-contain
               z-20
               w-1/2
-              lg:w-4/6
+              xl:w-4/6
               max-h-3/4
               transform
               -rotate-12
               translate-x-4
-              lg:translate-x-12
-              xl:translate-x-16
+              lg:translate-x-40
             "
             src="/contact/OSXDBL1_2.svg"
           />
