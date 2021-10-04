aeb193db7076ce7012c1b64dc543ead2e1d56f4e
RonaldRusso94 <RonaldRusso94@yahoo.com>
2021-10-04 19:15:22 -0400

    Add logo

--- components/NavBar/NavLogo.vue
+++ components/NavBar/NavLogo.vue
@@ -1,6 +1,7 @@
 <template>
   <!-- Logo should link to home -->
   <nuxt-link to="/" class="flex items-end cursor-pointer tracking-tighter">
+    <img class="h-12 mr-3" src="/LimeLightLogo.png" />
     <p class="font-bold text-4xl text-app-green-1">Lime</p>
     <p class="text-4xl text-app-green-1">Light</p>
     <p class="pl-4 text-app-green-1 opacity-75">Devs</p>
