e06f185ee815515adb609afb5d973b3ba8c7a013
RonaldRusso94 <RonaldRusso94@yahoo.com>
2021-10-04 21:22:23 -0400

    Logo SVG update

--- components/NavBar/NavLogo.vue
+++ components/NavBar/NavLogo.vue
@@ -1,11 +1,16 @@
 <template>
   <!-- Logo should link to home -->
   <nuxt-link to="/" class="flex items-end cursor-pointer tracking-tighter">
-    <img class="h-12 mr-3" src="/LimeLightLogo.png" />
+    <lime-light-logo class="h-12" />
     <p class="font-bold text-4xl text-app-green-1">Lime</p>
     <p class="text-4xl text-app-green-1">Light</p>
     <p class="pl-4 text-app-green-1 opacity-75">Devs</p>
   </nuxt-link>
 </template>
-<script></script>
+<script>
+import LimeLightLogo from '~/static/contact/LimeLightLogo.vue'
+export default {
+  components: { LimeLightLogo },
+}
+</script>
 <style></style>
