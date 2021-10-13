90f4a2c535d2b15570b61d193738b4bc1a555d56
RonaldRusso94 <RonaldRusso94@yahoo.com>
2021-10-13 13:39:28 -0400

    Css Fix for logo / nav

--- components/NavBar/NavLogo.vue
+++ components/NavBar/NavLogo.vue
@@ -1,14 +1,16 @@
 <template>
   <!-- Logo should link to home -->
-  <nuxt-link to="/" class="flex items-end cursor-pointer tracking-tighter">
-    <lime-light-logo class="h-12" />
-    <p class="font-bold text-4xl text-app-green-1">Lime</p>
-    <p class="text-4xl text-app-green-1">Light</p>
-    <p class="pl-4 text-app-green-1 opacity-75">Devs</p>
+  <nuxt-link to="/" class="flex items-center cursor-pointer tracking-tighter">
+    <lime-light-logo class="h-10 md:h-12 mr-4" />
+    <div class="flex items-end">
+      <p class="font-bold text-4xl text-app-green-1">Lime</p>
+      <p class="text-4xl text-app-green-1">Light</p>
+      <p class="ml-2 text-app-green-1 opacity-75">Devs</p>
+    </div>
   </nuxt-link>
 </template>
 <script>
-import LimeLightLogo from '~/static/contact/LimeLightLogo.vue'
+import LimeLightLogo from '~/static/LimeLightLogo.vue'
 export default {
   components: { LimeLightLogo },
 }
